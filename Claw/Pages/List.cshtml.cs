3b2db3a6f9e0f6cbc6c8374ebd2a86ed9beb0be1
jier38 <jier38@gmail.com>
2022-06-29 12:49:39 +0800

    update

--- Claw/Pages/List.cshtml.cs
+++ Claw/Pages/List.cshtml.cs
@@ -22,22 +22,24 @@ namespace Claw.Pages
 			{
 				Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 				clientSocket.Connect(new IPEndPoint(IPAddress.Parse("3.20.235.8"), 7771));
+								
+				string s = "{{\"cmd\":\"req_roomlist\"}}";
+				byte[] jsoncmd = Encoding.UTF8.GetBytes(s);
+				byte[] smsg = new byte[3 + jsoncmd.Length];
+				smsg[0] = (byte)0xda;
+				smsg[1] = (byte)(jsoncmd.Length / 256);
+				smsg[2] = (byte)(jsoncmd.Length % 256);
+				jsoncmd.CopyTo(smsg, 3);
+				clientSocket.Send(smsg);
 
 				byte[] date = new byte[1024];
-				//int count = clientSocket.Receive(date);
-				//string msg = Encoding.UTF8.GetString(date, 0, count);
-				//Console.WriteLine(msg);
-
-				string s = "{{\"cmd\":\"req_roomlist\"}}";
-
-				clientSocket.Send(Encoding.UTF8.GetBytes(s));
-
 				int count = clientSocket.Receive(date);
 				string msg = Encoding.UTF8.GetString(date, 0, count);
 				Console.WriteLine(msg);
 
 				clientSocket.Close();
-				
+
+
 
 				Rooms = new List<string>();
 				Rooms.Add("Machine 1");
